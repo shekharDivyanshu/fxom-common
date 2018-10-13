074b4c8050d5ecef8b14559b6f0215763d760851
divyanshu Shekhar <dshekhar@divyanshus-MacBook-Pro.local>
2018-10-13 02:00:55 -0400

    Initial project

---
+++ src/main/java/com/statestreet/fxom/fxomcommon/FxomCommonApplication.java
@@ -0,0 +1,12 @@
+package com.statestreet.fxom.fxomcommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FxomCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FxomCommonApplication.class, args);
+	}
+}
